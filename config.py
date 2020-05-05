c574b100a254dc30fd4f867c6fa71eb65fe15836
charliewp <cprice9739@carolina.rr.com>
2020-05-05 16:00:03 -0400

    move parms to secrets.py

--- config.py
+++ config.py
@@ -2,19 +2,20 @@
 
 import machine
 import ubinascii
+import secrets
 
 WIFI_MAC = ubinascii.hexlify(machine.unique_id()).upper()
 # Set  the Gateway ID to be the first 3 bytes of MAC address + 'FFFE' + last 3 bytes of MAC address
 GATEWAY_ID = WIFI_MAC[:6] + "FFFE" + WIFI_MAC[6:12]
 
-SERVER = 'meshio.net'
-PORT = 1700
+#SERVER = ''
+#PORT = 1700
 
 NTP = "pool.ntp.org"
 NTP_PERIOD_S = 3600
 
-WIFI_SSID = 'linville5'
-WIFI_PASS = 'thr3adtr4il'
+#WIFI_SSID = ''
+#WIFI_PASS = ''
 
 # for EU868
 #LORA_FREQUENCY = 868100000
